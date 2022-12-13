f01afea8b98a4a6652c1027498175634f7a28f35
Lou CHANCRIN <lchancri@e3r2p10.clusters.42paris.fr>
2022-12-13 15:22:32 +0100

    Completed project

---
+++ src/engine.py
@@ -0,0 +1,95 @@
+import sys
+from termcolor import colored
+from utils.error import NoSolutionError, NoTransitionError, InfiniteLoopError
+
+sys.setrecursionlimit(10000)
+
+def update_tape(tape: str, head: str, write: str):
+    if tape[-1] != '.':
+        return f"{tape[0:head]}{write}{tape[head+1:]}."    
+    return f"{tape[0:head]}{write}{tape[head+1:]}"
+
+def print_tape(tape:str, head: str, current_state: str, transition: object):
+    new_tape = f"{tape[0:head]}{colored(tape[head], 'green')}{tape[head+1:]}"
+    if tape[head] != 'X':
+        new_tape = new_tape.replace("X", colored("X", "yellow"))
+    if tape[head] != 'Y':
+        new_tape = new_tape.replace("Y", colored("Y", "red"))
+    if tape[head] != 'Q':
+        new_tape = new_tape.replace("Q", colored("Q", "blue"))
+    if tape[head] != 'Z':
+        new_tape = new_tape.replace("Z", colored("Z", "red"))
+    print(
+        f"[{new_tape}]"
+        f"  |  ({current_state}, {tape[head]}) ->"
+        f" ({transition[0]['to_state']},"
+        f" {transition[0]['write']}, {transition[0]['action']})")
+
+def get_action(head, action):
+    if action == "RIGHT":
+        return head + 1
+    if action == "LEFT":
+        return head - 1
+    return head
+
+def check_known_states(known_states, tape, head, current_state):
+    if tape in known_states:
+        if current_state in known_states[tape]:
+            if tape[head] in known_states[tape][current_state]:
+                if head in known_states[tape][current_state][tape[head]]:
+                    raise InfiniteLoopError(f"Infinite loop detected")
+                else:
+                    known_states[tape][current_state][tape[head]].append(head)
+            else:
+                known_states[tape][current_state][tape[head]] = [head]
+        else:
+            known_states[tape][current_state] = {tape[head]: [head]}
+    else:
+        known_states[tape] = {current_state: {tape[head]: [head]}}
+    return known_states
+
+def perform_transition(
+    machine: object,
+    tape: list,
+    head: int,
+    current_state: str,
+    transition: object,
+    count: int,
+    silent: bool,
+    known_states: object
+):
+    if len(transition) != 1:
+        raise NoTransitionError(f"Expected 1 transition for state {current_state} with read {tape[head]}, got {len(transition)}")
+    if not silent:
+        print_tape(tape, head, current_state, transition)
+    
+    return engine(
+        machine,
+        transition[0]["to_state"],
+        update_tape(tape, head, transition[0]["write"]),
+        get_action(head, transition[0]["action"]),
+        count + 1,
+        silent,
+        check_known_states(known_states, tape, head, current_state)
+    )
+
+def engine(machine: object, current_state: str, tape: list, head: int, count: int, silent: bool=False, known_states: object={}):
+    if current_state in machine["finals"]:
+        return count
+    if head == len(tape) or head < 0:
+        raise NoSolutionError("no solution")
+    return perform_transition(
+        machine,
+        tape,
+        head,
+        current_state,
+        list(
+            filter(
+                lambda transition: transition["read"] == tape[head],
+                machine["transitions"][current_state],
+            )
+        ),
+        count,
+        silent,
+        known_states
+    )
---
+++ src/ft_turing.py
@@ -0,0 +1,92 @@
+"""ft turing"""
+
+import argparse
+from json import JSONDecodeError, load as json_load
+import itertools
+
+from engine import engine
+from universal_engine import run_engine
+from utils.error import ParsingError, MachineError
+from utils.health_check import checks
+from utils.plot import plot_complexity
+
+def generate_strings(chars, len):
+    return [''.join(x) for x in itertools.product(chars, repeat=len)]
+
+def get_input():
+    """
+    Get arguments input
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("jsonfile", help="json description of the machine")
+    parser.add_argument("input", help="input of the machine", type=str)
+    parser.add_argument("--universal", dest="universal", action=argparse.BooleanOptionalAction, help="Used to compute universal machine", type=bool, default=False)
+    return parser.parse_args()
+
+def bonus():
+    files_path = {
+        '0n1n': 'machines_json/0n1n.json',
+        '02n': 'machines_json/02n.json',
+        'palindrome': 'machines_json/palindrome.json',
+        'unary_add': 'machines_json/unary_add.json'
+    }
+    max_complexity = {'0n1n': [0], '02n': [0], 'palindrome': [0], 'unary_add': [0]}
+    for key in files_path:
+        with open(files_path[key], encoding="utf-8") as machine_description:
+            machine = json_load(machine_description)
+        for i in range(1, 10):
+            no_blank_alphabet = [x for x in machine['alphabet'] if x not in machine['blank']]
+            inputs = generate_strings(no_blank_alphabet, i)
+            complexity = []
+            for single_input in inputs:
+                try:
+                    complexity.append(engine(
+                        machine=machine,
+                        current_state=machine["initial"],
+                        tape=(single_input + "."),
+                        head=0,
+                        count=0,
+                        silent=True
+
+                    ))
+                except:
+                    pass
+            if complexity:
+                max_complexity[key].append(max(complexity))
+            else:
+                max_complexity[key].append(0)
+    plot_complexity(max_complexity)
+
+if __name__ == "__main__":
+    args = get_input()
+    bonus()
+    import sys
+    sys.exit()
+    try:
+        machine, tape = checks(args)
+        if args.universal:
+            x = {
+                'machine': machine,
+                'current_state': machine["initial"],
+                'tape': (tape + "."),
+                'head': 0,
+            }
+            run_engine(x)
+        else:
+            engine(
+                machine=machine,
+                current_state=machine["initial"],
+                tape=(tape + "."),
+                head=0,
+                count=0
+            )
+    except JSONDecodeError as e:
+        print("JSON format error")
+        print(e)
+    except ParsingError as e:
+        print(e)
+    except MachineError as e:
+        print(f"Error : {e}")
+    except RecursionError:
+        print("Maximum recursion exceeded, use --universal argument")
